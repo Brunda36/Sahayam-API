889aef76ea76bbcab9b1ba873be33679ac7db6b8
Brunda36 <71182529+Brunda36@users.noreply.github.com>
2020-09-12 23:14:27 +0530

    express api

---
+++ data.js
@@ -0,0 +1,11 @@
+const data = [
+    {
+    id:1,
+    name:"abcd",
+},
+{
+    id:2,
+    name:"qwert",
+},
+]
+module.exports = data
---
+++ index.js
@@ -0,0 +1,35 @@
+const path = require("path");
+var express = require('Express');
+var bodyParser = require('body-parser');
+var data = require('./data');
+
+var app = express();
+
+app.set("view engine", "pug");
+app.set("views","./public")
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+   res.render("index", {title: "Search"});
+})
+
+app.post("/result", (req, res) => {
+   const found = (data.some(data => data.id === parseInt(req.body.id)));
+   if(found){
+      res.json(data.filter(data=>{
+         if(data.id === parseInt(req.body.id)){
+            res.send(`Name: `+data.name)
+         }      
+      }));
+   }else{
+      res.send("Data not found");
+   }
+})
+
+app.listen(4200, () => {
+    console.log("Starting express");
+})
+
